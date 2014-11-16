308988bf803c96e47125eb78340afd5f5002f1ef
vijay <vijay.k.vepakomma@gmail.com>
2014-11-16 17:52:10 -0500

    Added sanitize

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,6 +1,8 @@
 'use strict';
 
 eventsApp.controller('EventController', function EventController($scope){
+    $scope.snippet='<span style="color:red">hi there</span>';
+    $scope.boolValue = true;
     $scope.event = {
         name: 'Angular Boot Camp',
         date: '1/1/2013',
